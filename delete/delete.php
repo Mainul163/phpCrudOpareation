a650f581d0a7e90ad9dc69fff798a66035b41998
unknown <mainulisraf163@gmail.com>
2023-05-02 16:18:56 +0600

    delete done

--- delete/delete.php
+++ delete/delete.php
@@ -1,3 +1,38 @@
-<?php
-    echo"delete";
+<?php 
+ 
+ $connect=mysqli_connect("localhost","root","","crud");
+
+
+ if(!$connect){
+
+    die("database not connected").mysqli_error($connect); 
+ }else{
+
+     $rcv=$_REQUEST['id'];  
+     $query ="DELETE  FROM user_info WHERE id=$rcv";
+     $run_delete_query=mysqli_query($connect,$query);
+
+     if($run_delete_query){
+
+     header("location:index.php?deleted");
+
+     }else{
+        echo "something is wrong" ;
+     }
+
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
--- delete/index.php
+++ delete/index.php
@@ -1,4 +1,17 @@
-<?php
+<!DOCTYPE html>
+<html>
+<style>
+table,
+th,
+td {
+    border: 1px solid black;
+}
+</style>
+
+<body>
+
+    <h2>A basic HTML table</h2>
+    <?php
     $connect = mysqli_connect("localhost","root","","crud");
 
     if(!$connect){
@@ -11,29 +24,57 @@
 
        $count=mysqli_num_rows($result);
 
-       if($count){
-      
-        while($rows=mysqli_fetch_assoc($result)){
-            echo "<br>{$rows['id']}"."<br>"."<br>"."{$rows['username']}"."<br>";
+       if($count>0){
+        
+        if(isset($_REQUEST['deleted'])){
 
+            echo "<h3>Data deleted</h3>";
+        }
+        
+      ?>
+
+    <table style="width:100%">
+        <tr>
+            <th>id</th>
+            <th>username</th>
+            <th>email</th>
+            <th>action</th>
+        </tr>
+        <?php
+        while($rows=mysqli_fetch_assoc($result)){
+          
+
+
+      
+      ?>
+
+        <tr>
+            <td><?php echo  $rows['id'] ;?></td>
+            <td><?php echo  $rows['username'] ;?></td>
+            <td><?php echo  $rows['email'] ;?></td>
+            <td><a href="delete.php?id=<?php echo $rows['id'] ?>">Delete</a></td>
+        </tr>
+
+        <?php    
+ 
         }
 
 
        }else{
 
-            echo"database not connected";
+            echo"database not connected us";
 
        }
-      
+ ?>
 
-
-
+    </table>
+    <?php
 
     }
 
-
-
-
-
-    
 ?>
+
+
+</body>
+
+</html>
