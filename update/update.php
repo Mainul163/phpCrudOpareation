0faab6b169f7164783de9b483c4091ad13537446
Mainul islam <mainulisraf163@gmail.com>
2023-05-06 01:06:13 +0600

    update working

--- update/update.php
+++ update/update.php
@@ -10,16 +10,35 @@
 
    $recvID=$_REQUEST['id'];
    
-   $query="SELECT * FROM user_info WHERE id= $recvID"
-
+   $query="SELECT * FROM user_info WHERE id= $recvID";
+   $update=mysqli_query($connection,$query);
    
+   while($row=mysqli_fetch_assoc($update)){
+?>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method='post'>
+    <label for="fname">user name:</label><br>
+    <input type="text" value="<?php echo $row['username']; ?>" name="username" id="fname"><br><br>
+    <label for="lname">email:</label><br>
+    <input type="text" value="<?php echo $row['email']; ?>" name="email" id="lname"><br><br>
+    <label for="lname">password:</label><br>
+    <input type="text" value="<?php echo $row['password']; ?>" name="password" id="lname"><br><br>
+
+    <input type="submit" value="Submit" name='submit'>
+</form>
 
 
- }
+
+<?php
+
+   }
+   
+  
+
+}
 
 
 
 
 
-    
+
 ?>
